c112c68ee1864f4c8073a7bc9ab3ba5581207f28
NicolasAbuin99 <nicolasabuin19@gmail.com>
2025-11-03 20:44:37 -0300

    Add files via upload

---
+++ Trabajo-Practico-Nro2/Nicolas-Abuin/src/main/java/trabajo_practico_nr2/vehiculo/Coche.java
@@ -0,0 +1,26 @@
+package trabajo_practico_nr2.vehiculo;
+import lombok.Data;
+
+@Data
+public class Coche extends Vehiculo  {
+    private int puertas;
+
+    public Coche(String marca, String modelo, double precio, int puertas) {
+        super(marca, modelo, precio);
+        this.puertas = puertas;
+    }
+
+
+    @Override
+    public String toString(){
+        
+        String precioSinSigno = String.format("%,.2f", getPrecio());
+        //Toma el numero y lo convierte a string con el formato %,.2f
+        String precioFinal = precioSinSigno.replace(',', 'X').replace('.', ',').replace('X', '.');
+        //Realiza tres reemplazos para intercambiar la coma y el punto de lugar
+        return "Marca: " + getMarca() + 
+               " // Modelo: " + getModelo() + 
+               " // Puertas: " + this.puertas + 
+               " // Precio:  $" + precioFinal;
+    }
+}
